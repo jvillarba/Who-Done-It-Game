69ef29ffeb5db720832a3d644c202a73a0c4bab4
Joe Villarba <socagambit@aol.com>
2016-02-02 23:25:33 -0800

    more images, working game

--- app.js
+++ app.js
@@ -34,79 +34,130 @@ var weapons = [
 var killer = {}
 var playerGuess = {}
 
-// pressing "Reset"
-startButton.addEventListener('click', function(){
-  console.log('reset game now')
+var game = {
+  player1: {
+    name: "Player 1",
+    score: 0
+  },
+  player2: {
+    name: "Player 2",
+    score: 0
+  }
+}
 
-// randomizes pictures
+// FROM BRYAN FROM PHILIPPE HOW TO SWITCH BETWEEN PLAYERS
+var currentPlayer = game.player1
+
+function switchTurn(){
+  if(currentPlayer == game.player1){
+    currentPlayer = game.player2
+    console.log(currentPlayer)
+  } else {
+    currentPlayer = game.player1
+    console.log(currentPlayer)
+  }
+  return currentPlayer
+}
+
+  // ADDS SCORING FUNCTION
+  // function incrementScore(){
+  //   currentPlayer.score += 1
+  // }
+
+// CLICKING START BUTTON
+startButton.addEventListener('click', function(){
+    console.log('start game now')
+
+// SHUFFLES THE PICTURES ARRAY
   function fisherYates (myArray) {
-    var i = myArray.length;
-    if ( i == 0 ) return false;
-    while ( --i ) {
-       var j = Math.floor( Math.random() * ( i + 1 ) );
-       var tempi = myArray[i];
-       var tempj = myArray[j];
-       myArray[i] = tempj;
-       myArray[j] = tempi;
+      var i = myArray.length;
+      if ( i == 0 ) return false;
+      while ( --i ) {
+        var j = Math.floor( Math.random() * ( i + 1 ) );
+        var tempi = myArray[i];
+        var tempj = myArray[j];
+        myArray[i] = tempj;
+        myArray[j] = tempi;
      }
   }
 
-// // random generator
+// random generator
 //     function pickRandom(x){
 //       x = Math.floor(Math.random() * boxes.length)
 //       return x
 //     }
-// lays out the choices -- need to work on not overlapping
-    for (var i = 0; i < boxes.length; i++) {
-      fisherYates(pictures)
-      boxes[i].innerHTML = pictures.splice(0,1)
+
+// LAYS OUT THE CHOICES ON THE BOARD
+      for (var i = 0; i < boxes.length; i++) {
+        fisherYates(pictures)
+        boxes[i].innerHTML = pictures.splice(0,1)
     }
 
-// clears killer variable
+// CLEARS KILLER OBJECT FOR NEW GAME
   killer = {}
 
-// computer randomly picks a killer and a weapon
+// COMPUTER RANDOMLY PICKS KILLER & WEAPON
   killer.suspect = suspects[Math.floor(Math.random() * suspects.length)]
   killer.weapon = weapons[Math.floor(Math.random() * weapons.length)]
 
-  instructions.innerHTML = "Who do you think did it?"
-
-// console.log(killer)
+// DISPLAYS KILLER
   console.log(killer)
 
+// PLAYER CHOOSES A SUSPECT
+  instructions.innerHTML = "Pick a suspect"
+
   for (var i = 0; i < suspectCards.length; i++) {
-    suspectCards[i].addEventListener('click', function(){
-    playerGuess.suspect = (this.id)
-    instructions.innerHTML = "You chose " + playerGuess.suspect +". Now what weapon did they use."
-
-    // if (this.id === killer.suspect.id) {
-    //   console.log('you got em')
-    //   return
-    // }
-    //   console.log('wrong. try again')
-    })
+      suspectCards[i].addEventListener('click', function(){
+      playerGuess.suspect = (this.id)
+      instructions.innerHTML = "You chose " + playerGuess.suspect +". Now, what weapon do you think they used?"
+      })
   }
+  // if (this.id === killer.suspect.id) {
+  //   console.log('you got em')
+  //   return
+  // }
+  //   console.log('wrong. try again')
 
+// PLAYER CHOOSES A WEAPON
   for (var i = 0; i < weaponCards.length; i++) {
-    weaponCards[i].addEventListener('click', function(){
-    playerGuess.weapon = (this.id)
-    instructions.innerHTML = "You chose the " + playerGuess.weapon
-    // if (this.id === killer.weapon.id) {
-    //   console.log('right weapon')
-    //   return
-    // }
-    //   console.log('wrong. try again')
-    })
+      weaponCards[i].addEventListener('click', function(){
+      playerGuess.weapon = (this.id)
+      instructions.innerHTML = "You chose the " + playerGuess.weapon
+      getWinner()
+      switchTurn()
+      })
+
+  }
+  // if (this.id === killer.weapon.id) {
+  //   console.log('right weapon')
+  //   return
+  // }
+  //   console.log('wrong. try again')
+
+
+  //
+
+  var getWinner = function() {
+    if (playerGuess.suspect === killer.suspect.id && playerGuess.weapon === killer.weapon.id){
+      instructions.innerHTML = "You win"
+    } else if ((playerGuess.suspect === killer.suspect.id && playerGuess.weapon !== killer.weapon.id) || (playerGuess.suspect !== killer.suspect.id && playerGuess.weapon === killer.weapon.id)){
+      instructions.innerHTML = "You have one right"
+    } else {
+      instructions.innerHTML = "Sorry, try again"
+    }
   }
 
+    //   if (isWinner1()) {
+    //     return 'player1';
+    //   }
+    //   if (isWinner2()) {
+    //     return 'player2';
+    //   }
+    //   return null;
+    //
 
-
-  // if (playerGuess.suspect === killer.suspect.id && playerGuess.weapon === killer.weapon.id){
-  //   instructions.innerHTML = "You win"
-  // } else if ((playerGuess.suspect === killer.suspect.id && playerGuess.weapon !== killer.weapon.id) || (playerGuess.suspect !== killer.suspect.id && playerGuess.weapon === killer.weapon.id)){
-  //   instructions.innerHTML = "You have one right"
-  // } else {
-  //   instructions.innerHTML = "Sorry, try again"
-  //   }
   //
+  // while (getWinner !== true) {
+  //   startButton.innerText="Reset Game"
+  // }
 })
